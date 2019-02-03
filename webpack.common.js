0765c5002b3a2036e6ece849ba17f9aef0cdecb2
Neville Li <nevilleneville@hotmail.com>
2019-02-02 22:05:56 -0600

    reconfigure webpack

--- webpack.common.js
+++ webpack.common.js
@@ -3,13 +3,8 @@ const path = require("path");
 module.exports = {
     entry: "./src/app.js",
     output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    mode: "development",
-    devtool: "cheap-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "public")
+        filename: "bundle.js",
+        path: path.join(__dirname, "public")
     },
     watchOptions: {
         ignored: /node_modules/
---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const path = require("path");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "cheap-eval-source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "public")
+    }
+})
